cb2e585cdf8d285727f82dedae524a2ce8f18e9e
Min Kim <mk4555@nyu.edu>
2018-02-26 19:28:48 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to '/'
     else
       render 'new'
