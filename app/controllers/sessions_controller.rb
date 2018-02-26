61ee62903d8230001ceb57f791bcc455d23fec2f
Min Kim <mk4555@nyu.edu>
2018-02-26 19:27:57 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,15 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    if @user.save
+      redirect_to '/'
+    else
+      render 'new'
+    end
   end
 
   def user_params
     params.require(:user).permit(:name, :password)
   end
-  
+
 end
